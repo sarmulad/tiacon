56677041dfb8d50e565d03183eb75488d8d78f74
sarmulad <resolvetech2020@gmail.com>
2023-04-28 16:12:25 +0100

    tokenomics uncheckedr

--- src/components/TokenomicsChart.tsx
+++ src/components/TokenomicsChart.tsx
@@ -1,45 +1,45 @@
-import React from 'react';
-import { Doughnut } from 'react-chartjs-2';
-import { Chart, DoughnutController } from 'chart.js/auto';
+// import React from 'react';
+// import { Doughnut } from 'react-chartjs-2';
+// import { Chart, DoughnutController } from 'chart.js/auto';
 
-Chart.register(DoughnutController);
+// Chart.register(DoughnutController);
 
-type TokenomicsProps = {
-  data: number[];
-  labels: string[];
-};
+// type TokenomicsProps = {
+//   data: number[];
+//   labels: string[];
+// };
 
-const TokenomicsChart: React.FC<TokenomicsProps> = ({ data, labels }) => {
-  const chartData = {
-    labels,
-    datasets: [
-      {
-        data,
-        backgroundColor: ['#ff6384', '#36a2eb', '#ffce56', '#8ec46b', '#58304E', '#B3B486','#8ec46b'],
-        borderWidth: 0,
-      },
-    ],
-  };
-  
-// @ts-ignore
+// const TokenomicsChart: React.FC<TokenomicsProps> = ({ data, labels }) => {
+//   const chartData = {
+//     labels,
+//     datasets: [
+//       {
+//         data,
+//         backgroundColor: ['#ff6384', '#36a2eb', '#ffce56', '#8ec46b', '#58304E', '#B3B486','#8ec46b'],
+//         borderWidth: 0,
+//       },
+//     ],
+//   };
 
-  const options = {
-    responsive: true,
-    maintainAspectRatio: false,
-    plugins: {
-      legend: {
-        display: true,
-        position: 'bottom',
-        labels: {
-          font: {
-            size: 10,
-          },
-        },
-      },
-    },
-  };
+// // @ts-ignore
 
-  return <Doughnut data={chartData} options={options} />;
-};
+//   const options = {
+//     responsive: true,
+//     maintainAspectRatio: false,
+//     plugins: {
+//       legend: {
+//         display: true,
+//         position: 'bottom',
+//         labels: {
+//           font: {
+//             size: 10,
+//           },
+//         },
+//       },
+//     },
+//   };
 
-export default TokenomicsChart;
+//   return <Doughnut data={chartData} options={options} />;
+// };
+
+// export default TokenomicsChart;
